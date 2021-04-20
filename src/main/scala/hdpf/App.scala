21e3b6d481fd811f9788a052bcdaeb9a8f7ff2c1
zhang18668 <894218668@qq.com>
2021-04-20 23:38:23 +0800

    flink watermark

--- src/main/scala/hdpf/App.scala
+++ src/main/scala/hdpf/App.scala
@@ -3,12 +3,10 @@ package hdpf
 import hdpf.bean.{Device, Message, Participant, Payload}
 import hdpf.operator.{AllWindowApply, IsInPloyin}
 import hdpf.utils.FlinkUtils
-import hdpf.watermark.{AssginerWaterMark, StrategyWaterMark}
-import javafx.util.Duration
+import hdpf.watermark.StrategyWaterMark
 import org.apache.flink.api.common.eventtime.WatermarkStrategy
 import org.apache.flink.api.scala._
 import org.apache.flink.streaming.api.scala.DataStream
-import org.apache.flink.streaming.api.watermark.Watermark
 import org.apache.flink.streaming.api.windowing.assigners.{SlidingProcessingTimeWindows, TumblingEventTimeWindows}
 import org.apache.flink.streaming.api.windowing.time.Time
 import java.time.Duration
@@ -26,7 +24,8 @@ object App {
     val kafkaDataStream: DataStream[String] = env.addSource(consumer)
     val canalDs = kafkaDataStream.map(json => Message(json))
     //添加水印
-    val waterDs: DataStream[Message] = canalDs.assignTimestampsAndWatermarks(WatermarkStrategy.forBoundedOutOfOrderness[Message](java.time.Duration.ofSeconds(20)).withTimestampAssigner(new StrategyWaterMark))
+    val strategy: WatermarkStrategy[Message] = WatermarkStrategy.forBoundedOutOfOrderness[Message](Duration.ofSeconds(20)).withTimestampAssigner(new StrategyWaterMark)
+    val waterDs: DataStream[Message] = canalDs.assignTimestampsAndWatermarks(strategy)
     //链
     val payloadDS: DataStream[Payload] = waterDs.map(message => Payload(message.payload))
     val deviceDS: DataStream[Array[Device]] = payloadDS.map(_.device_data)
@@ -34,11 +33,11 @@ object App {
     val devMapDS: DataStream[Array[Participant]] = devDS.map(_.`object`)
     val parDS: DataStream[Participant] = devMapDS.flatMap(y => y)
     val arrFilter: DataStream[Participant] = parDS.filter(new IsInPloyin)
-    val winDS: DataStream[Int] = arrFilter.windowAll(SlidingProcessingTimeWindows.of(Time.seconds(60),Time.seconds(10))).apply(new AllWindowApply)
+    val winDS: DataStream[Int] = arrFilter.windowAll(SlidingProcessingTimeWindows.of(Time.seconds(60), Time.seconds(10))).apply(new AllWindowApply)
 
 
 
     // 执行任务
-    env.execute("sync-db")
+    env.execute("hdpf_bigdata")
   }
 }
