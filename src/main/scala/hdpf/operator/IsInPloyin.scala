35628b3edd0b6247274cbcaa16e75e1e9e2de9a6
Zhangzhihua <zhangzhihua@cmsr.chinamobile.com>
2021-04-30 16:22:07 +0800

    flink streaming

--- src/main/scala/hdpf/operator/IsInPloyin.scala
+++ src/main/scala/hdpf/operator/IsInPloyin.scala
@@ -27,7 +27,18 @@ class IsInPloyin extends FilterFunction[Participant] {
   override def filter(value: Participant): Boolean = {
     var flag=true
     val p = Point(value.location.longitude, value.location.latitude)
-    val pts = List(Point(121.612389396, 31.2522492584), Point(121.612510129, 31.2522890327), Point(121.612447139, 31.2524226077), Point(121.612350552, 31.2523838254))
+
+
+//    路口1 对应的group.id="intersection01"  tablename="intersection01"
+//    val pts = List(Point(121.612389396, 31.2522492584), Point(121.612510129, 31.2522890327), Point(121.612447139, 31.2524226077), Point(121.612350552, 31.2523838254))
+//    路口2 对应的group.id="intersection02"  tablename="intersection02"
+//    val pts = List(Point(121.61205607, 31.2529267832), Point(121.612196226, 31.252972781), Point(121.612247669, 31.2528658129), Point(121.612108563, 31.2528162074))
+//    道路road01 对应的group.id="road01"  tablename="road01"  CDJI
+//    val pts = List(Point(121.612447139, 31.2524226077), Point(121.612389397, 31.2524009617), Point(121.612247669,31.252842363), Point(121.612247669, 31.2528658129))
+//    车道lane01(直行北向) 对应的group.id="lane01"  tablename="lane01" COPI
+//    val pts = List(Point(121.612447139, 31.2524226077), Point(121.612418268, 31.2524117847), Point(121.612219323, 31.25285408795), Point(121.612247669, 31.2528658129))
+//    车道lane02(直行北向) 对应的group.id="lane02"  tablename="lane02"  DOPJ
+    val pts = List(Point(121.612389397, 31.2524009617), Point(121.612196226, 31.252972781), Point(121.612247669, 31.2528658129), Point(121.612190977, 31.252842363))
 
     var intersection = 0
     for (i <- pts.indices) {
--- src/main/scala/hdpf/utils/FlinkUtils.scala
+++ src/main/scala/hdpf/utils/FlinkUtils.scala
@@ -18,23 +18,23 @@ object FlinkUtils {
     env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime)
 //    env.setStreamTimeCharacteristic(TimeCharacteristic.ProcessingTime)
     //  设置并行度
-    env.setParallelism(3)
+//    env.setParallelism(3)
 //
-//    // 设置checkpoint
-//    // 开启checkpoint,间隔时间为5s
-//    env.enableCheckpointing(5000)
-//    // 设置处理模式
-//    env.getCheckpointConfig.setCheckpointingMode(CheckpointingMode.EXACTLY_ONCE)
-////    // 设置两次checkpoint的间隔
-//    env.getCheckpointConfig.setMinPauseBetweenCheckpoints(1000)
-////    // 设置超时时长
-//    env.getCheckpointConfig.setCheckpointTimeout(60000)
-//    // 设置并行度
-//    env.getCheckpointConfig.setMaxConcurrentCheckpoints(1)
-//    // 当程序关闭的时候,触发额外的checkpoint
-//    env.getCheckpointConfig.enableExternalizedCheckpoints(CheckpointConfig.ExternalizedCheckpointCleanup.RETAIN_ON_CANCELLATION)
-//    // 设置检查点在hdfs中存储的位置
-//    env.setStateBackend(new FsStateBackend("hdfs://cdh01:8020/flink-checkpoint"))
+    // 设置checkpoint
+    // 开启checkpoint,间隔时间为5s
+    env.enableCheckpointing(5000)
+    // 设置处理模式
+    env.getCheckpointConfig.setCheckpointingMode(CheckpointingMode.EXACTLY_ONCE)
+    // 设置两次checkpoint的间隔
+    env.getCheckpointConfig.setMinPauseBetweenCheckpoints(1000)
+    // 设置超时时长
+    env.getCheckpointConfig.setCheckpointTimeout(60000)
+    // 设置并行度
+    env.getCheckpointConfig.setMaxConcurrentCheckpoints(1)
+    // 当程序关闭的时候,触发额外的checkpoint
+    env.getCheckpointConfig.enableExternalizedCheckpoints(CheckpointConfig.ExternalizedCheckpointCleanup.RETAIN_ON_CANCELLATION)
+    // 设置检查点在hdfs中存储的位置
+    env.setStateBackend(new FsStateBackend("hdfs://cdh01:8020/flink-checkpoint"))
 
 
 
