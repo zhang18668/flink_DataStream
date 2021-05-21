6db617da4d8be5b281ce376634367b1d3b62d6f1
Lin Liu <liu.lin@outlook.com>
2021-05-21 10:52:45 +0800

    Add files via upload

--- src/main/scala/hdpf/bean/source/Location.scala
+++ src/main/scala/hdpf/bean/source/Location.scala
@@ -3,23 +3,16 @@ package hdpf.bean.source
 import com.alibaba.fastjson.JSON
 
 case class Location(
-                 var vehicle_judge: String,
-                 var longitude: Double,
-                 var latitude: Double,
-                 var altitude: Double,
-                 var headroom: Int
-               )
+                     var vehicle_judge: String,
+                     var longitude: Double,
+                     var latitude: Double,
+                     var altitude: Double,
+                     var headroom: Int
+                   )
 
 object Location {
   def apply(json: String): Location = {
     JSON.parseObject[Location](json, classOf[Location])
-  }
-
-  def main(args: Array[String]): Unit = {
-    val json = "{\"vehicle_judge\":\"false\",\"longitude\":114.07174935388105,\"latitude\":30.449874911928906,\"altitude\":-0.7003295662911274,\"headroom\":0}"
-    val loca = Location(json)
-
-    println(loca.vehicle_judge)
   }
 }
 
--- src/main/scala/hdpf/bean/source/Pose.scala
+++ src/main/scala/hdpf/bean/source/Pose.scala
@@ -3,20 +3,13 @@ package hdpf.bean.source
 import com.alibaba.fastjson.JSON
 
 case class Pose(
-                  var length: Double,
-                  var width: Double,
-                  var height: Double
-                )
+                 var length: Double,
+                 var width: Double,
+                 var height: Double
+               )
 
 object Pose {
   def apply(json: String): Pose = {
     JSON.parseObject[Pose](json, classOf[Pose])
-  }
-
-  def main(args: Array[String]): Unit = {
-    val json = "{\"length\":4.3034539222717289,\"width\":2.033831834793091,\"height\":1.4431898593902588}"
-    val pose = Pose(json)
-
-    println(pose.length)
   }
 }
